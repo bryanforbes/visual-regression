f97a29b8b12956933d99796284db3b01b1df814a
Paul Shannon <pshannon@sitepen.com>
2016-11-02 09:39:54 -0700

    Updated ci environments

--- tests/intern.ci.ts
+++ tests/intern.ci.ts
@@ -1,14 +1,15 @@
 export const capabilities = {
-	'browserstack.selenium_version': '2.45.0'
+	'selenium-version': '2.43.0',
+	'idle-timeout': 30
 };
 
 export const environments = [
-	{ browserName: 'internet explorer', version: '11', platform: 'WIN8' },
-	{ browserName: 'internet explorer', version: '10', platform: 'WIN8' },
-	{ browserName: 'internet explorer', version: '9', platform: 'WINDOWS' },
-	{ browserName: 'firefox', version: '37', platform: [ 'WINDOWS', 'MAC' ] },
-	{ browserName: 'chrome', version: '39', platform: [ 'WINDOWS', 'MAC' ] },
-	{ browserName: 'safari', version: '8', platform: 'MAC' }
+	{ browserName: 'internet explorer', version: '11.0', platform: 'Windows 10' },
+	{ browserName: 'internet explorer', version: '10.0', platform: 'Windows 8' },
+	{ browserName: 'internet explorer', version: '9.0', platform: 'Windows 7' },
+	{ browserName: 'firefox', version: '33.0', platform: [ 'Windows 7', 'OS X 10.11' ] },
+	{ browserName: 'chrome', version: '38.0', platform: [ 'Windows 7', 'OS X 10.11' ] },
+	{ browserName: 'safari', version: '9.0', platform: 'OS X 10.11' }
 ];
 
 export const maxConcurrency = 2;
